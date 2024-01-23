97b744e68bb77edabdd369af6ca92fc462010148
Chiến Phạm <22110116@student.hcmute.edu.vn>
2024-01-23 14:00:43 +0700

    update

---
+++ Hello/HocSinh.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class HocSinh
+    {
+        public string name;
+        public void SetValue()
+        {
+            this.name = " Chien";
+        }
+    }
+}
