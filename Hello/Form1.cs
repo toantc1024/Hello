bfcae9a1e6acf8793565b9d34de08ebee361d539
VanThanh <ongvanthanh9a5xt@gmail.com>
2024-01-23 14:25:56 +0700

    aaaaaaaaaa

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -10,7 +10,7 @@ namespace Hello
         private void btnClick_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Clicked!");
-            int a = 5;
+            int a = 8;
         }
 
         private void Form1_Load(object sender, EventArgs e)
