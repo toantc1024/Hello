cf3d72641619cedecf04a062a7398d49472a3b1b
Ánh Ngọc Bùi Thị <angoc1234567890@gmail.com>
2024-01-23 13:55:10 +0700

    thêm student22110191

---
+++ Hello/Student22110191.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Student22110191
+    {
+        public string name = "BuiThiAnhNgoc";
+        public string id = 22110191;
+    }
+}
