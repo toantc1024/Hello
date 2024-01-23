ea723c0193b699aa94e5a938cd49819ecd40d1f8
Nguyen Tuan Kiet <tuankietnguyen1399@gmail.com>
2024-01-23 14:16:03 +0700

    update

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -10,7 +10,7 @@ namespace Hello
         private void btnClick_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Clicked!");
-            int a = 3;
+            int a = 7;
         }
 
         private void Form1_Load(object sender, EventArgs e)
