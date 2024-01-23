3289460e6b6172671d1b9b0b5b99b250b1bb91a2
Nguyen Tuan Kiet <tuankietnguyen1399@gmail.com>
2024-01-23 14:24:30 +0700

    Revert "update"
    This reverts commit f275732cc4823e3d11144b5047fa18b3f7d86eb1.

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -10,7 +10,7 @@ namespace Hello
         private void btnClick_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Clicked!");
-            int a = 100;
+            int a = 9;
         }
 
         private void Form1_Load(object sender, EventArgs e)
