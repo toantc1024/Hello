1f1ae507c073c9a3cbb4e13794ae619a21db1bf4
Quân Phạm <phamanhquantv2016@gmail.com>
2024-01-23 14:27:29 +0700

    bgbgbg

---
+++ Hello/HocSinh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class HocSinh
+    {
+        int a = 1;
+        int b = 2;
+        int c = a + base;gvvv
+        Console.WriteLn
+    }
+}
