a5635671555ab8d26727348895aa1332bd179a58
for2024 <phamchung1.qtkd@gmail.com>
2024-01-23 14:00:19 +0700

    ADD CLASS

---
+++ Hello/HocSinh.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class HocSinh
+    { 
+        int a;
+        int b;
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        
+    }
+    
+}
