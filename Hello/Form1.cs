cbb13defc4a22254ab29d9947fb99ad2dbac9041
NhatAnh0702 <nhatanh0024@gmail.com>
2024-01-23 13:41:38 +0700

    Update checkbox student or not student

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -1,4 +1,4 @@
-namespace Hello
+﻿namespace Hello
 {
     public partial class Form1 : Form
     {
@@ -16,6 +16,16 @@ namespace Hello
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                // Hiển thị message box khác
+                MessageBox.Show("You uncheck Student!");
+            }
+            else MessageBox.Show("You choose Student!");
         }
     }
 }
