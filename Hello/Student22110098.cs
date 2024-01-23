0c3d5ee31806d07a3288039d146a526f45a90538
HaiAnh251 <149868011+HaiAnh251@users.noreply.github.com>
2024-01-23 13:58:56 +0700

    add information

--- Hello/Student22110098.cs
+++ Hello/Student22110098.cs
@@ -8,6 +8,19 @@ namespace Hello
 {
     internal class Student22110098
     {
-        public string name = "Nguyen Ngoc Hai Anh";
+        private string name;
+        private int age;
+        private int id;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public int Id { get => id; set => id = value; }
+
+        public Student22110098() 
+        {
+            Name = "Nguyen Ngoc Hai Anh";
+            Age = 18;
+            Id = 22110098;
+        }
     }
 }
