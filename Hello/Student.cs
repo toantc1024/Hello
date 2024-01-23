ae58033bba80c280590fadfb159ab95dfe12b8c9
Quốc Bảo Nguyễn <nguyenquocbaopl321@gmail.com>
2024-01-23 14:08:27 +0700

    Update: Add class student

---
+++ Hello/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class Student
+    {
+        public string name = "Nguyen Quoc Bao";
+    }
+}
