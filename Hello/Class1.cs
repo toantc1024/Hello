e26bf8462dd772f804e5934e58b7baf772de155f
mayyyitn <tnn03754@gmail.com>
2024-01-23 14:01:28 +0700

    Add Inf4 DTan

---
+++ Hello/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Student_22110225
+    {
+        private string Name;
+        private int age;
+        private int id;
+
+        public string Name1 { get => Name; set => Name = value; }
+        public int Age { get => age; set => age = value; }
+        public int Id { get => id; set => id = value; }
+        public Student_22110225() 
+        {
+            Name = "Duy Tan";
+            Age = 19;
+            id = 22110225;
+        }
+    }
+}
