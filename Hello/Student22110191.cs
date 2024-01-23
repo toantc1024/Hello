65d8a83ac9d515f270c5a5297c0847e3b9fa1804
Ánh Ngọc Bùi Thị <angoc1234567890@gmail.com>
2024-01-23 14:06:14 +0700

    sua thongtin

--- Hello/Student22110191.cs
+++ Hello/Student22110191.cs
@@ -8,7 +8,16 @@ namespace Hello
 {
     internal class Student22110191
     {
-        public string name = "BuiThiAnhNgoc";
-        public string id = "22110191";
+        private string Ten;
+        private int Tuoi;
+
+        public string Ten1 { get => this.Ten; set => this.Ten = value; }
+        public int Tuoi1 { get => Tuoi; set => Tuoi = value; }
+
+        public Student22110191()
+        {
+            Ten = "AnhNgoc";
+            Tuoi = 20;
+        }
     }
 }
