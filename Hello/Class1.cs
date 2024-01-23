871602c3a0108250cc586e6d06623e3a724e87c8
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-01-23 13:55:35 +0700

    update: add new class

---
+++ Hello/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class ToanTranCong
+    {
+        private int a = 3;
+    }
+
+
+    // todo : Merge all branches
+
+    // one -> pull other branch fix conflicts
+}
